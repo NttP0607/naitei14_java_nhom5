dd088d96d7ad245e8dae827a1c15e471d394c5fc
thanh <thanhadp2402@gmail.com>
2025-11-30 10:39:21 +0700

    update send email when upload application

--- src/main/java/vn/sun/public_service_manager/controller/ApplicationController.java
+++ src/main/java/vn/sun/public_service_manager/controller/ApplicationController.java
@@ -10,9 +10,13 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import vn.sun.public_service_manager.dto.response.FileResDTO;
+import vn.sun.public_service_manager.dto.response.MailResDTO;
 import vn.sun.public_service_manager.entity.Application;
+import vn.sun.public_service_manager.entity.Citizen;
 import vn.sun.public_service_manager.exception.FileException;
+import vn.sun.public_service_manager.repository.CitizenRepository;
 import vn.sun.public_service_manager.service.ApplicationService;
+import vn.sun.public_service_manager.service.EmailService;
 import vn.sun.public_service_manager.utils.FileUtil;
 import vn.sun.public_service_manager.utils.SecurityUtil;
 import vn.sun.public_service_manager.utils.annotation.ApiMessage;
@@ -23,10 +27,15 @@ public class ApplicationController {
 
     private final FileUtil fileUtil;
     private final ApplicationService applicationService;
+    private final EmailService emailService;
+    private final CitizenRepository citizenRepository;
 
-    public ApplicationController(FileUtil fileUtil, ApplicationService applicationService) {
+    public ApplicationController(FileUtil fileUtil, ApplicationService applicationService, EmailService emailService,
+            CitizenRepository citizenRepository) {
         this.fileUtil = fileUtil;
         this.applicationService = applicationService;
+        this.emailService = emailService;
+        this.citizenRepository = citizenRepository;
     }
 
     @PostMapping("/upload")
@@ -45,6 +54,8 @@ public class ApplicationController {
 
         // create user folder if not exists
         String username = SecurityUtil.getCurrentUserName();
+        Citizen citizen = citizenRepository.findByNationalId(username)
+                .orElseThrow(() -> new FileException("Citizen with national ID " + username + " not found."));
         fileUtil.createDirectoryIfNotExists(username);
 
         // save files to user folder
@@ -52,6 +63,17 @@ public class ApplicationController {
 
         // save application data
         Application application = applicationService.createApplication(serviceId, note, files);
+
+        MailResDTO mailResDTO = new MailResDTO();
+        mailResDTO.setApplicationCode(application.getApplicationCode());
+        mailResDTO.setServiceName(application.getService().getName());
+        mailResDTO.setSubmittedAt(application.getSubmittedAt());
+        // send mail
+        emailService.sendApplicationConfirmationEmail(
+                citizen.getEmail(),
+                citizen.getFullName(), "Hồ sơ đã đăng ký...",
+                mailResDTO,
+                "email_template");
 
         FileResDTO response = new FileResDTO();
         response.setApplicationId(application.getApplicationCode());
