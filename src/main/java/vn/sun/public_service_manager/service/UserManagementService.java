36c49bce507c09525c93f1ee29e179c6da0434a2
NttP0607 <166805936+NttP0607@users.noreply.github.com>
2025-12-23 15:33:06 +0700

    Revert "fix export csv  application"

--- src/main/java/vn/sun/public_service_manager/service/UserManagementService.java
+++ src/main/java/vn/sun/public_service_manager/service/UserManagementService.java
@@ -88,6 +88,9 @@ public interface UserManagementService {
 
     void exportCitizensToCsv(Writer writer);
 
+    void exportApplicationsToCsv(Writer writer);
+
+    void exportApplicationsToCsv(Writer writer, org.springframework.security.core.Authentication authentication);
 
     public Map<String, Object> importCitizensFromCsv(MultipartFile file) throws IOException;
 
